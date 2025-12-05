42b5ddd7ff720468d4ec78dc956a1fd5ecd9f82d
AL4255 <92372109+AL4255@users.noreply.github.com>
2025-12-05 18:03:57 -0500

    Update server.c

--- server.c
+++ server.c
@@ -1,1 +1,9 @@
 
+#include <stdio.h>      // Standard input/output functions (printf, etc.)
+#include <stdlib.h>     // Standard library (exit, malloc, etc.)
+#include <string.h>     // String manipulation functions (strlen, strcpy, etc.)
+#include <unistd.h>     // UNIX standard functions (close, read, write, etc.)
+#include <sys/socket.h> // Socket functions (socket, bind, listen, accept)
+#include <netinet/in.h> // Internet protocol family structures
+#include <sys/wait.h>   // For wait() function to handle child processes
+#include <signal.h>     // For signal handling (cleaning up zombies)
